3f51018e7342e3ec9c8ed836ab1f6a19b40aa33b
roberte777 <robewilkes@yahoo.com>
2023-07-23 17:49:48 -0500

    updated doc settings and added github actions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = [".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
